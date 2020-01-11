9aeb946e1d208560bbe38980b932ade817ebdcce
bjoern <bjoern@carcerem.lan.xhain.space>
2020-01-11 16:59:10 +0100

    hardcode ssh pubkeys

--- config/target.exs
+++ config/target.exs
@@ -6,22 +6,13 @@ config :xdoor,
   authorized_keys_update_interval_ms: 60 * 60 * 1000,
   gpio_enabled: true
 
-keys =
-  [
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
-  ]
-  |> Enum.filter(&File.exists?/1)
-
-if keys == [],
-  do:
-    Mix.raise("""
-    No SSH public keys found in ~/.ssh. An ssh authorized key is needed to
-    log into the Nerves device and update firmware on it using ssh.
-    See your project's config.exs for this error message.
-    """)
+keys = [
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMBSfSmc2s5m8HpuSxyD2LP0FgpyYDs7oan/lfdwN9sZ",
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFk68ujMEgPVglDNnxqrht/0piGwofQy4GmPjgq4CvUV"
+]
 
 config :nerves_firmware_ssh,
-  authorized_keys: Enum.map(keys, &File.read!/1)
+  authorized_keys: keys
 
 config :nerves_network, :default,
   wlan0: [
