97eb746e475af27ff2629ac8a2c2ba9abae078ae
reimerei <git@bjrm.de>
2020-03-11 21:55:09 +0100

    send report when lock state changes. -> Preparation for new implemention of door bot

--- lib/xdoor/lock_state.ex
+++ lib/xdoor/lock_state.ex
@@ -4,6 +4,7 @@ defmodule Xdoor.LockState do
 
   @poll_frequency_ms 100
   @gpio_lock_sensor 8
+  @report_url "http://nas:8000/door"
 
   def locked?() do
     Application.get_env(:xdoor, :lock_state)
@@ -38,10 +39,15 @@ defmodule Xdoor.LockState do
 
     if last_state != current_state do
       Logger.info("Locked? state changed from :#{last_state} to #{current_state}")
-      log(current_state)
+      on_state_change(current_state)
 
       Application.put_env(:xdoor, :lock_state, current_state)
     end
+  end
+
+  defp on_state_change(state) do
+    log(state)
+    send_report(state)
   end
 
   defp log(state) do
@@ -52,5 +58,22 @@ defmodule Xdoor.LockState do
 
     IO.puts(file, "#{DateTime.utc_now() |> DateTime.to_iso8601()};#{state}")
     File.close(file)
+  end
+
+  defp send_report(state) do
+    body =
+      %{
+        state: state,
+        last_motion_s: (Xdoor.MotionDetection.last_motion() / 1000) |> round
+      }
+      |> Jason.encode!()
+
+    case Mojito.post(@report_url, [], body) do
+      {:ok, reponse} ->
+        Logger.debug("Lock state change report send. response: #{inspect(reponse)}")
+
+      error ->
+        Logger.error("Error sending lock stage change report: #{inspect(error)}")
+    end
   end
 end
